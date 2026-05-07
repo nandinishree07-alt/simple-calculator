6ca65790eff5b970260b719b40dc54dc67d0d8ff
nandinishree07-alt <nandinishree07@gmail.com>
2026-05-07 11:33:31 +0530

    Initial commit - Simple CLI Calculator

---
+++ README.md
@@ -0,0 +1,19 @@
+# Simple Calculator CLI
+
+## Objective
+A simple command-line calculator built using Python.
+
+## Features
+- Addition
+- Subtraction
+- Multiplication
+- Division
+- Invalid input handling
+
+## Technologies Used
+- Python
+
+## How to Run
+
+```bash
+python calculator.py
---
+++ calculator.py
@@ -0,0 +1,56 @@
+# Simple CLI Calculator
+
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    if b == 0:
+        return "Error: Cannot divide by zero"
+    return a / b
+
+
+while True:
+    print("\n===== SIMPLE CALCULATOR =====")
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+    print("5. Exit")
+
+    choice = input("Enter your choice (1-5): ")
+
+    if choice == "5":
+        print("Calculator Closed")
+        break
+
+    if choice not in ["1", "2", "3", "4"]:
+        print("Invalid choice. Please try again.")
+        continue
+
+    try:
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+
+        if choice == "1":
+            print("Result:", add(num1, num2))
+
+        elif choice == "2":
+            print("Result:", subtract(num1, num2))
+
+        elif choice == "3":
+            print("Result:", multiply(num1, num2))
+
+        elif choice == "4":
+            print("Result:", divide(num1, num2))
+
+    except ValueError:
+        print("Invalid input. Please enter numbers only.")
